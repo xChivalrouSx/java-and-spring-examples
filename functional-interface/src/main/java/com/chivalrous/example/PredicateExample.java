069ca20ad9bff9be0d60cda93792c352efd0a6d3
Mert Çakar <mertcakar.es@gmail.com>
2024-05-05 22:24:31 +0300

    Java Supplier and Predicate examples are added.

---
+++ functional-interface/src/main/java/com/chivalrous/example/PredicateExample.java
@@ -0,0 +1,34 @@
+package com.chivalrous.example;
+
+import java.util.function.Predicate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PredicateExample {
+
+	@SuppressWarnings("java:S4276")
+	private static final Predicate<String> isEmailValid = value -> value.indexOf("@") != -1 &&
+			value.indexOf("@") != 0 && value.indexOf("@") != value.length() - 1;
+
+	@SuppressWarnings("java:S4276")
+	private static final Predicate<String> isEducationalEmail = value -> value.indexOf("@") != -1 &&
+			value.indexOf(".edu") != -1 && value.indexOf(".edu") > value.indexOf("@");
+
+	public static void main(String[] args) {
+		log.info("'isEmailValid' predicate result for 'test@test.com': {}", String.valueOf(isEmailValid.test("test@test.com")));
+		log.info("'isEmailValid' predicate result for '@test.com': {}", String.valueOf(isEmailValid.test("@test.com")));
+		log.info("'isEmailValid' predicate result for 'test@': {}", String.valueOf(isEmailValid.test("test@")));
+		log.info("'isEmailValid' predicate result for 'test-email-dot-com': {}", String.valueOf(isEmailValid.test("test-email-dot-com")));
+
+		log.info("'isEmailValid' predicate negate result for 'test@test.com': {}", String.valueOf(isEmailValid.negate().test("test@test.com.tr")));
+
+		log.info("'isEmailValid' and 'isEducationalEmail' predicate result for 'test@test.com.tr': {}",
+				String.valueOf(isEmailValid.and(isEducationalEmail).test("test@test.com.tr")));
+		log.info("'isEmailValid' and 'isEducationalEmail' predicate result for 'test@test.edu.tr': {}",
+				String.valueOf(isEmailValid.and(isEducationalEmail).test("test@test.edu.tr")));
+		log.info("'isEmailValid' and 'isEducationalEmail' predicate result for 'test.edu@test.com': {}",
+				String.valueOf(isEmailValid.and(isEducationalEmail).test("test.edu@test.com")));
+	}
+
+}
---
+++ functional-interface/src/main/java/com/chivalrous/example/SupplierExample.java
@@ -0,0 +1,51 @@
+package com.chivalrous.example;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SupplierExample {
+
+	@SuppressWarnings("java:S4276")
+	private static final Supplier<Person> createPerson = Person::new;
+
+	@SuppressWarnings("java:S4276")
+	private static final Supplier<Integer> generateNewAge = Person::getRandomAge;
+
+	public static void main(String[] args) {
+		log.info("Created: " + createPerson.get().toString());
+		log.info("Created: " + new Person("new-name", "new-surname", generateNewAge.get()));
+	}
+
+	@Getter
+	@AllArgsConstructor
+	private static class Person {
+
+		private static Random random = new Random();
+
+		private String name;
+		private String surname;
+		private int age;
+
+		public Person() {
+			name = "default-name";
+			surname = "default-surname";
+			age = 25;
+		}
+
+		public static int getRandomAge() {
+			return random.nextInt(70) + 1;
+		}
+
+		@Override
+		public String toString() {
+			return "Person [name=" + name + ", surname=" + surname + ", age=" + age + "]";
+		}
+
+	}
+
+}
